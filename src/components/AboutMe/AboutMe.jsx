9501d48c2dcea8d302a45dc6466ae88c8db22500
= <=>
2026-06-20 23:25:06 +0500

    h1 change

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -22,7 +22,7 @@ const AboutMe = () => {
           As a Junior Frontend Developer, I possess an impressive arsenal of skills in <span>HTML5</span> , <span>CSS3</span>, <span>JavaScript</span>, <span>ReactJS</span>. I excel in designing and maintaining <span>responsive websites</span> that offer a smooth user experience. My expertise lies in crafting <span>dynamic</span>, engaging interfaces through writing <span>clean</span> and <span>optimized code</span> and utilizing cutting-edge development tools and techniques. I am also a <span>team player</span> who thrives in collaborating with cross-functional teams to produce outstanding web applications.
         </p>
         
-        <span>My name is Ayan Shoukat Ali. I want to seek Front End Development</span>
+        <h1>My name is Ayan Shoukat Ali. I want to seek Front End Development</h1>
 
 
 
