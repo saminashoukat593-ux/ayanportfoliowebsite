7b2262d0b4ff5944589b972e2b61a9318aedcbbf
= <=>
2026-07-10 11:51:08 +0500

    picture change

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './projectSection.css'
 
-import projectpicture from '../../assets/image.png'
+import projectpicture from '../../assets/image.jpeg'
 
 const ProjectSection = () => {
 
